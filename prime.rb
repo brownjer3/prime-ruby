bdcd7e8afc5e64666a219dfe49777b280f94604f
jerry <brownjer3@gmail.com>
2020-10-22 14:00:26 -0700

    commit tree a8481419b1d9b5909f39c7f86eaf32c9b6891fc2

--- prime.rb
+++ prime.rb
@@ -1,1 +1,46 @@
-# Add  code here!
+require 'benchmark'
+require 'bigdecimal/math'
+require 'pry'
+
+#example = rand(-1000000..1000000)
+
+def prime?(int)
+    if int <= 1
+        false
+    elsif int == 2 
+        true
+    else
+        numbers = (2..(int-1)).to_a
+        numbers.none? do |i|
+            int % i == 0 
+        end
+    end
+end
+
+
+# i'm thinking this could be faster by trying a different iterator -- .find ?
+
+=begin
+def prime_2?(int)
+    if int <= 1
+        false
+    elsif int == 2 
+        true
+    else
+        numbers = (2..(int-1)).to_a
+        numbers.find do |i|
+            return false if int % i == 0 
+        end
+        true
+    end
+end
+
+
+
+Benchmark.bm(10) do |x|
+    x.report("None in range: ") { prime? example }
+    x.report("Find in range ") { prime_2? example }
+end
+
+# they ended up being both very similar
+=end 
